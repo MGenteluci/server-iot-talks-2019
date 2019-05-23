d47ba29879f32e7e5068a430afad6cc8685177ff
Matheus Genteluci <mgenteluci97@gmail.com>
2019-05-22 21:30:35 -0300

    Add validation that startDate must be b4 endDate

--- src/event/event.middleware.js
+++ src/event/event.middleware.js
@@ -2,28 +2,31 @@ const moment = require('moment');
 
 exports.validatePayload = (req, res, next) => {
   if (!req.body.startDate)
-    return res.status(400).json({ message: 'missing startDate' });
+    return res.status(400).json({ error: 'missing startDate' });
 
   if (!req.body.endDate)
-    return res.status(400).json({ message: 'missing endDate' });
+    return res.status(400).json({ error: 'missing endDate' });
 
   if (!req.body.openArchTime)
-    return res.status(400).json({ message: 'missing openArchTime' });
+    return res.status(400).json({ error: 'missing openArchTime' });
 
   if (!req.body.machine)
-    return res.status(400).json({ message: 'missing machine' });
+    return res.status(400).json({ error: 'missing machine' });
 
   if (!req.body.voltage)
-    return res.status(400).json({ message: 'missing voltage' });
+    return res.status(400).json({ error: 'missing voltage' });
 
   if (!req.body.current)
-    return res.status(400).json({ message: 'missing current' });
+    return res.status(400).json({ error: 'missing current' });
 
   if (!moment(req.body.startDate).isValid())
-    return res.status(400).json({ message: 'startDate is not a valid Date' });
+    return res.status(400).json({ error: 'startDate is not a valid Date' });
 
   if (!moment(req.body.endDate).isValid())
-    return res.status(400).json({ message: 'endDate is not a valid Date' });
+    return res.status(400).json({ error: 'endDate is not a valid Date' });
+
+  if (moment(req.body.startDate).isAfter(req.body.endDate))
+    return res.status(400).json({ error: 'startDate must be before endDate' });
 
   return next();
 };
@@ -33,13 +36,13 @@ exports.validateDate = (req, res, next) => {
     const date = req.params.date;
 
     if (!moment(date).isValid())
-      return res.status(400).json({ message: `[${date}] is not a valid Date` });
+      return res.status(400).json({ error: `[${date}] is not a valid Date` });
 
     if (moment(date).isAfter(moment()))
-      return res.status(400).json({ message: `Date must not be after today` });
+      return res.status(400).json({ error: `Date must not be after today` });
 
     return next();
   } catch (err) {
-    return res.status(400).json({ message: `[${date}] is not a valid Date` });
+    return res.status(400).json({ error: `[${date}] is not a valid Date` });
   }
 };
