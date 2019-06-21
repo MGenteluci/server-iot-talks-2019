2ffae8186db9fc8f39ed0e133cdd92c2a6d0e873
Matheus Genteluci <mgenteluci97@gmail.com>
2019-06-20 21:13:03 -0300

    Hide ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,13 +4,13 @@ module.exports = {
     script: './bin/www',
     env: {
       NODE_ENV: 'production',
-      MONGO_URL: 'mongodb+srv://admin:admin@cluster0-5msyk.mongodb.net/iot-talks-2019?retryWrites=true&w=majority'
+      MONGO_URL: '{mongo-url}'
     }
   }],
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-35-172-193-18.compute-1.amazonaws.com',
+      host: '{host-url}',
       key: '~/.ssh/iot-talks-2019.pem',
       ref: 'origin/master',
       repo: 'git@github.com:MGenteluci/server-iot-talks-2019.git',
